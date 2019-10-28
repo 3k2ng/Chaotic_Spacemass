7b73f49b14002596cc76b851eabc4ccb62763cee
SADragonMaker <44631569+SADragonMaker@users.noreply.github.com>
2019-10-27 20:50:12 -0700

    point is fixed

--- src/Objects/AutoSpaceshipTest.gd
+++ src/Objects/AutoSpaceshipTest.gd
@@ -11,7 +11,7 @@ var general_speed: float = 1
 func _process(delta: float) -> void:
 	location = get_viewport().get_mouse_position()
 	
-	var dis := location - position
+	var dis := location - position - velocity.normalized() / (scale.length() / sqrt(2)) * 32
 	if dis.x > get_viewport_rect().size.x / 2:
 		location.x -= get_viewport_rect().size.x + border_gap / (scale.length() / sqrt(2))
 	if dis.y > get_viewport_rect().size.y / 2:
@@ -21,7 +21,7 @@ func _process(delta: float) -> void:
 	if dis.y < -get_viewport_rect().size.y / 2:
 		location.y += get_viewport_rect().size.y + border_gap / (scale.length() / sqrt(2))
 	
-	dis = location - position
+	dis = location - position - velocity.normalized() / (scale.length() / sqrt(2)) * 32
 	
 	general_speed = min(1, (dis.length() / scale.length()) / 128)
 	if general_speed < .064:
