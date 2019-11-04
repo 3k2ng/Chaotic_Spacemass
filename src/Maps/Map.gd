43bc97d10ef5c30b1f3620605753014848e86729
SADragonMaker <44631569+SADragonMaker@users.noreply.github.com>
2019-11-04 16:45:55 +0700

    Add transitions and maps
    coool maps

--- src/Maps/Map.gd
+++ src/Maps/Map.gd
@@ -1,11 +1,31 @@
 extends Area2D
 
+var scene : Node = Node.new()
+var players := preload("res://src/Scenes/TwoPlayer.tscn")
+export var packed : bool
 
+func _input_event(viewport, event, shape_idx):
+	if event.is_action_pressed("left-mouse") and packed:
+		var pl = players.instance()
+		var mp = self.duplicate()
+		mp.packed = false
+		mp.position = Vector2.ZERO
+		mp.scale = Vector2.ONE
+		scene.name = "Scene"
+		scene.add_child(mp)
+		scene.add_child(pl)
+		get_parent().get_parent().add_child(scene)
+		get_parent().queue_free()
+		pass
 
+func _mouse_entered():
+	if packed:
+		$AnimationPlayer.play("zoom_in")
+	else:
+		scale = Vector2.ONE
 
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _mouse_exited():
+	if packed:
+		$AnimationPlayer.play("zoom_out")
+	else:
+		scale = Vector2.ONE
--- src/Script/Start.gd
+++ src/Script/Start.gd
@@ -1,18 +1,8 @@
 extends Area2D
 
-var scene : Node = Node.new()
-var players := preload("res://src/Scenes/TwoPlayer.tscn")
-var map := preload("res://src/Maps/Map_1.tscn")
-
 func _input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("left-mouse"):
-		scene.name = "Scene"
-#		get_tree().change_scene("res://MapSelect.tscn")
-		scene.add_child(map.instance())
-		scene.add_child(players.instance())
-		get_parent().get_parent().add_child(scene)
-		get_parent().queue_free()
-		pass
+		get_tree().change_scene("res://MapSelect.tscn")
 
 func _mouse_entered():
 	$AnimationPlayer.play("zoom_in")
